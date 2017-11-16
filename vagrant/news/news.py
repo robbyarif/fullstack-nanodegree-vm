28167f9678fe95348584ca3f427b59d592658146
Robby Arifandri <>
2017-11-16 17:08:40 +0700

    Add get most popular authors function.

--- vagrant/news/news.py
+++ vagrant/news/news.py
@@ -2,10 +2,13 @@
 A report tool for news log
 """
 
-from newsdb import get_most_popular_articles, get_most_popular_authors
+from newsdb import (get_most_popular_articles,
+                    get_most_popular_authors,
+                    get_days_with_error_rate)
 
 
 
 if __name__ == '__main__':
     get_most_popular_articles(2)
     get_most_popular_authors(4)
+    get_days_with_error_rate(0.01)
--- vagrant/news/newsdb.py
+++ vagrant/news/newsdb.py
@@ -7,17 +7,17 @@ import datetime
 import psycopg2
 
 def get_most_popular_articles(number):
-    """Return all posts from the 'database', most recent first."""
+    """Return title and number views of the most popular articles from news log ordered from the
+     highest views."""
     conn = psycopg2.connect("dbname=news")
     cur = conn.cursor()
-    print("%s" % str(number))
     query = """SELECT title, COUNT(log.path) as views
         FROM articles
         LEFT JOIN log ON path LIKE '%'||slug
         GROUP BY title
         ORDER BY views DESC
         LIMIT {}""".format(number)
-    print (query)
+    print(query)
     cur.execute(query)
     rows = cur.fetchall()
     conn.close()
@@ -25,10 +25,39 @@ def get_most_popular_articles(number):
     return rows
 
 def get_most_popular_authors(number):
-    """Add a post to the 'database' with the current timestamp."""
+    """Return author name and number of views of the most popular authors from news log ordered from
+     highest views."""
+    conn = psycopg2.connect("dbname=news")
+    cur = conn.cursor()
+    query = """SELECT name, COUNT(log.path) as views
+        FROM authors
+        LEFT JOIN articles ON author = authors.id
+        LEFT JOIN log ON path LIKE '%'||slug
+        GROUP BY name
+        ORDER BY views DESC
+        LIMIT {}""".format(number)
+    print(query)
+    cur.execute(query)
+    rows = cur.fetchall()
+    conn.close()
+    print(rows)
+    return rows
+
+def get_days_with_error_rate(error_rate):
+    """Return the date and the error rate of the day with the most error ordered from the highest
+    error rate."""
     # conn = psycopg2.connect("dbname=news")
     # cur = conn.cursor()
-    # cur.execute("""INSERT INTO posts VALUES (%s) """, (number,))
-    # conn.commit()
+    # query = """SELECT title, COUNT(log.path) as views
+    #     FROM articles
+    #     LEFT JOIN log ON path LIKE '%'||slug
+    #     GROUP BY title
+    #     ORDER BY views DESC
+    #     LIMIT {}""".format(number)
+    # print(query)
+    # cur.execute(query)
+    # rows = cur.fetchall()
     # conn.close()
-    print('hello world no author listed yet WIP %s' % number)
+    rows = [('July 29, 2015', error_rate)]
+    print(rows)
+    return rows
