d68efac43ae0af853f982a7761ae63be666adf9f
Robby Arifandri <robby.arifandri@gmail.com>
2018-03-18 07:00:07 +0700

    Add json endpoint to db

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -14,12 +14,22 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
+    
+
 
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
 
 class Item(Base):
@@ -33,6 +43,16 @@ class Item(Base):
     created_datetime = Column(DateTime)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'created_time': self.created_datetime
+        }
 
 
 engine = create_engine('sqlite:///itemcatalog.db')
